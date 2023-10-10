eb7fccdcefe5de4c477d61c071e8b856a60e2142
DrewStroede <dstrow13@gmail.com>
2023-10-10 17:48:50 -0500

    figuring out api issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
 import Explorer from './components/Explorer';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 
-const API_KEY = 'pk.a45be98cbfd7d0adc47687e422c9a2d5';
-// const API_KEY = import.meta.env.LOCATIONIQ_API_KEY;
+// let API_KEY='pk.a45be98cbfd7d0adc47687e422c9a2d5';
+const API_KEY = import.meta.env.VITE_LOCATIONIQ_API_KEY;
 
 class App extends React.Component {
   constructor () {
     super ();
       this.state= {
         searchQuery: '',
-        location: null,
+        location: {},
       }
     }
 
@@ -21,8 +21,8 @@ setSearchQuery = (query) => {
   this.setState({ searchQuery: query });
 }
   handleForm = (e)=> {
-    console.log('form submitted');
     e.preventDefault();
+    console.log('API key:', API_KEY);
     axios.get(`https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${this.state.searchQuery}&format=json`)
     .then(response => {
       console.log('success', response.data);
@@ -36,7 +36,7 @@ setSearchQuery = (query) => {
 
   }
 render() {
-  console.log('CITY EXPLORER', this.state);
+
   return (
     <>
       <header>
@@ -47,18 +47,23 @@ render() {
         : <p>Please Enter A Valid Location</p>
         } */}
 
-        <BrowserRouter>
+        {/* <BrowserRouter> */}
           <form onSubmit={this.handleForm}>
-            <input placeholder="Enter City Name" type="text" name="city" onChange={this.handleChange} />
-            <button type= "submit">
-              <Link to="/search">Search!</Link>
+            <input placeholder="Enter City Name"
+             type="text"
+             name="city"
+             value= {this.state.searchQuery}
+             onChange={this.handleChange} />
+            <button type= "submit"> Search
+              {/* <Link to="/search">Search!</Link> */}
             </button>
           </form>
-          <Routes>
+          <Explorer location ={this.state.location} query= {this.state.searchQuery}/>
+          {/* <Routes>
             <Route exact path="/search" element={<Explorer location ={this.state.location} query= {this.state.searchQuery}/>} />
             <Route path="/" element={<p>Please enter a location to see results.</p>} />
           </Routes>
-        </BrowserRouter>
+        </BrowserRouter> */}
       </header>
       </>
   )
--- src/components/Explorer.jsx
+++ src/components/Explorer.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 // import weather from '../assets/data/weather.json';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+
+let API_KEY = import.meta.env.VITE_LOCATIONIQ_API_KEY;
 
 class Explorer extends React.Component {
 
     render() {
 
         let { location } = this.props;
+        let lat= location ? location.lat : '';
+        let lon= location ? location.lon : '';
+        let cityMap = `https://maps.locationiq/v3/staticmap?key=${API_KEY}&center=${lat},${lon}$zoom=9`;
+
         return (
             <main>
-                <Link to="/">Go Home</Link>
+                {/* <Link to="/">Go Home</Link> */}
                 <section>
                     <h2> Maps</h2>
                     <p>{this.props.query}</p>
-                    <p> Lat: {location ? location.lat: 'no location set'}</p>
-                    <p> Lon: {location ? location.lon: 'no location set'}</p>
-                    <img src={location ? location.map : "https//paceholder.co/600x400"}
+                    <img src={location ? cityMap : "https//paceholder.co/600x400"}
                     alt= "map of location"></img>
                 </section>
                 <section>
