c180766cccb95750388882bdbb499e8b0c6edbb0
DrewStroede <dstrow13@gmail.com>
2023-10-16 20:09:42 -0500

    figuring out API

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import './App.css';
 import Explorer from './components/Explorer';
 // import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import axios from 'axios';
+import { Button, Form } from 'react-bootstrap';
 
-// let API_KEY='pk.a45be98cbfd7d0adc47687e422c9a2d5';
+
+
 const API_KEY = import.meta.env.VITE_LOCATIONIQ_API_KEY;
 
 class App extends React.Component {
   constructor () {
     super ();
       this.state= {
+        displayContent: false,
         searchQuery: '',
         location: {},
       }
@@ -22,11 +25,10 @@ setSearchQuery = (query) => {
 }
   handleForm = (e)=> {
     e.preventDefault();
-    console.log('API key:', API_KEY);
     axios.get(`https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${this.state.searchQuery}&format=json`)
     .then(response => {
       console.log('success', response.data);
-      this.setState({ location: response.data[0] });
+      this.setState({ location: response.data[0], displayContent: true });
     }) .catch(error =>{
       console.log('error', error);
     });
@@ -36,7 +38,7 @@ setSearchQuery = (query) => {
 
   }
 render() {
-
+  console.log('CITY EXPLORER', this.state);
   return (
     <>
       <header>
@@ -46,24 +48,23 @@ render() {
         ? <Explorer /> 
         : <p>Please Enter A Valid Location</p>
         } */}
+          <Form onSubmit={this.handleForm}>
+            <input 
+              type= 'text'
+              name= 'city'
+              placeholder="Enter City Name"
+              onChange={this.handleChange} />
+            <Button 
+             type= 'submit'>
+              Explore!
+            </Button>
+          </Form>
+          {this.state.location.length > 0 &&
+          <Explorer
+           location ={this.state.location}
+           query= {this.state.searchQuery}/>
+          }
 
-        {/* <BrowserRouter> */}
-          <form onSubmit={this.handleForm}>
-            <input placeholder="Enter City Name"
-             type="text"
-             name="city"
-             value= {this.state.searchQuery}
-             onChange={this.handleChange} />
-            <button type= "submit"> Search
-              {/* <Link to="/search">Search!</Link> */}
-            </button>
-          </form>
-          <Explorer location ={this.state.location} query= {this.state.searchQuery}/>
-          {/* <Routes>
-            <Route exact path="/search" element={<Explorer location ={this.state.location} query= {this.state.searchQuery}/>} />
-            <Route path="/" element={<p>Please enter a location to see results.</p>} />
-          </Routes>
-        </BrowserRouter> */}
       </header>
       </>
   )
--- src/components/Explorer.jsx
+++ src/components/Explorer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import weather from '../assets/data/weather.json';
-// import { Link } from 'react-router-dom';
+
 
 let API_KEY = import.meta.env.VITE_LOCATIONIQ_API_KEY;
 
@@ -10,17 +10,27 @@ class Explorer extends React.Component {
 
         let { location } = this.props;
         let lat= location ? location.lat : '';
+        console.log(lat);
         let lon= location ? location.lon : '';
+        console.log(lon);
         let cityMap = `https://maps.locationiq/v3/staticmap?key=${API_KEY}&center=${lat},${lon}$zoom=9`;
 
         return (
             <main>
-                {/* <Link to="/">Go Home</Link> */}
+                
                 <section>
-                    <h2> Maps</h2>
-                    <p>{this.props.query}</p>
-                    <img src={location ? cityMap : "https//paceholder.co/600x400"}
-                    alt= "map of location"></img>
+                    <div className="card" style={{width: '30rem'}}>
+                        <img className="card-img-top" src={location ? cityMap : "https//paceholder.co/600x400"} alt="map of location"/>
+                            <div className="card-body">
+                                <h3 className="card-title">Map</h3>
+                                <p className="card-text">{this.props.query}</p>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">Lattitude = {this.props.location.lat}</li>
+                                <li className="list-group-item">Longitude = {this.props.location.lon}</li>
+                                <li className="list-group-item">Weather</li>
+                            </ul>
+                    </div>
                 </section>
                 <section>
                    {/* {weather.data.map(dailyForcast => {
