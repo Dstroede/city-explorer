f77a1dc98e055e35b0181a56bd1fdcce841fdb87
DrewStroede <dstrow13@gmail.com>
2023-10-28 15:50:55 -0500

    updated readme

--- src/App.jsx
+++ src/App.jsx
@@ -3,87 +3,88 @@ import './App.css';
 import Explorer from './components/Explorer.jsx';
 // import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import axios from 'axios';
-import { Button,Container, Form } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
 import Alert from 'react-bootstrap/Alert';
 
 
 class App extends React.Component {
-  constructor (props) {
-    super (props);
-      this.state= {
-        searchQuery: '',
-        displayContent: false,
-        location: {},
-        error: null, 
-        weather: [],
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: '',
+      displayContent: false,
+      location: {},
+      error: null,
+      weather: [],
     }
+  }
 
 
-setSearchQuery = (query) => {
-  this.setState({ searchQuery: query });
-}
-  handleForm = (e)=> {
+  setSearchQuery = (query) => {
+    this.setState({ searchQuery: query });
+  }
+  handleForm = (e) => {
     e.preventDefault();
     axios.get(`https://us1.locationiq.com/v1/search?key=${import.meta.env.VITE_LOCATIONIQ_API_KEY}&q=${this.state.searchQuery}&format=json`)
-    .then(response => {
-      let city = response.data[0];
-      console.log('success', response.data[0]);
-      this.setState({ location: city, displayContent: true, error: null });
-      this.showWeather(city.lat, city.lon)
-    }) .catch(error =>{
-      this.setState({error: error});
-      console.log('error', error);
-    });
+      .then(response => {
+        let city = response.data[0];
+        this.setState({ location: city, displayContent: true, error: null });
+        this.showWeather(city.lat, city.lon)
+      }).catch(error => {
+        if (error.response) {
+          console.log(error.response.data);
+          this.setState({ error: `Server Error: ${error.response.status} - ${error.response.data.error}` });
+        } else {
+          this.setState({ error: 'An error has occurred while making the request' });
+        }
+      });
   }
 
   showWeather = async (lat, lon) => {
-    console.log(lat, lon);
     const res = await axios.get(`http://localhost:3001/weather?lat=${lat}&lon=${lon}`)
-    console.log('here is res data', res.data)
-    this.setState({weather: res.data})
+    this.setState({ weather: res.data })
   }
-render() {
-  return (
-    <>
-      <Container>
-        <h1> Welcome to City Explorer</h1>
+  render() {
+    return (
+      <>
+        <Container>
+          <h1> Welcome to City Explorer</h1>
           <Form onSubmit={this.handleForm}>
             <Form.Control
-              onChange={(e) => this.setState({searchQuery: e.target.value})}
-              type= 'text'
-              name= 'city'
+              onChange={(e) => this.setState({ searchQuery: e.target.value })}
+              type='text'
+              name='city'
               placeholder="Enter City Name"
-             />
-            <Button 
-             type= 'submit'>
+            />
+            <Button
+              type='submit'>
               Explore!
             </Button>
           </Form>
           {this.state.error && (
-            <Alert variant= 'danger'>
+            <Alert variant='danger'>
               {`${this.state.error}`}
             </Alert>
           )}
           {Object.keys(this.state.location).length > 0 &&
-          <>
-          <Explorer
-           location = {this.state.location}
-           locationName ={this.state.location.display_name}
-           lon= {this.state.location.lon}
-           lat= {this.state.location.lat}
-           query= {this.state.searchQuery}
-           forecast= {this.state.weather}
-           />
-           </>
+            <>
+              <Explorer
+                location={this.state.location}
+                locationName={this.state.location.display_name}
+                lon={this.state.location.lon}
+                lat={this.state.location.lat}
+                query={this.state.searchQuery}
+                forecast={this.state.weather}
+              />
+            </>
           }
           <>
 
 
           </>
-      </Container>
+        </Container>
       </>
-  )
+    )
   }
 }
 export default App
